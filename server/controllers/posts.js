b4f277d28f228b58c32f786c891f98e678548064
DMWebDev <89517604+SuperDMWebDev@users.noreply.github.com>
2022-08-18 18:12:28 +0700

    change

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,7 +1,19 @@
-export const getPost = (req, res) => {
-  res.send('This work');
+const postMessage = require('../models/postMessage');
+export const getPost = async (req, res) => {
+  try {
+    const postMessage = await PostMessage.find();
+    console.log(postMessage);
+
+    res.status(200).json(postMessage);
+  } catch (err) {
+    res.status(404).json({ message: err.message });
+  }
 };
 
 export const createPost = (req, res) => {
-  res.send();
+  const body = req.body;
+  try {
+  } catch (err) {
+    console.log('err', err);
+  }
 };
